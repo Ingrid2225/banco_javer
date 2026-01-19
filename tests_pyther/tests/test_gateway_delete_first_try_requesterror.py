d51bd9b064a3c853c9ff0ef665f17e5617795d0b
Ingrid Lorrane <109640862+Ingrid2225@users.noreply.github.com>
2026-01-19 15:54:01 -0300

    Update test_gateway_delete_first_try_requesterror.py

--- tests_pyther/tests/test_gateway_delete_first_try_requesterror.py
+++ tests_pyther/tests/test_gateway_delete_first_try_requesterror.py
@@ -6,7 +6,7 @@ import httpx
 async def test_delete_desativar_requesterror_primeiro_try(api_async_client):
     client, fake = api_async_client
 
-    # 1º try: obter_conta -> RequestError (mapa 503 no except de cima)
+   
     async def boom_obter(ag, num):
         raise httpx.RequestError("unavailable", request=httpx.Request("GET", "http://x"))
     fake.obter_conta = boom_obter
